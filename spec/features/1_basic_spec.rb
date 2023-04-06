3e9df368976da83ec3a91196e21c488fe9600c89
Ben Purinton <ben.purinton@gmail.com>
2023-04-06 15:53:20 -0800

    devise authenticate_user spec

--- spec/features/1_basic_spec.rb
+++ spec/features/1_basic_spec.rb
@@ -1,16 +1,26 @@
 require "rails_helper"
 
-describe "/users/sign_in" do
+describe "User authentication with the Devise gem" do
   let(:user) { User.create(username: "alice", email: "alice@example.com", password: "password") }
 
-  it "allows a user to sign in", points: 2 do
-    visit new_user_session_path
+  it "requires sign in before any action with the Devise `before_action :authenticate_user!` method", points: 2 do
+    visit "/#{user.username}"
+    current_url = page.current_path
 
-    fill_in "Email", with: user.email
-    fill_in "Password", with: user.password
-    click_button "Log in"
-
-    expect(page).to have_content("Signed in successfully."),
-      "Expected to visit /users/sign_in, login with a signed up user, and be redirected to the homepage with a notification 'Signed in successfully.'."
+    expect(current_url).to eq(new_user_session_path),
+      "Expected `before_action :authenticate_user!` in `ApplicationController` to redirect guest to /users/sign_in before visiting another page."
   end
+end
+
+def sign_in_user
+  user = User.create(username: "alice", email: "alice@example.com", password: "password")
+  visit new_user_session_path
+
+  fill_in "Email", with: user.email
+  fill_in "Password", with: user.password
+  click_button "Log in"
+end
+
+def user_model_exists?
+  Object.const_defined?("User")
 end
