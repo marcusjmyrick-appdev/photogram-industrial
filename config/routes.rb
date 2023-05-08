69ee6a26e74bcdf9895918085c69ee15d4cb94df
Marcus Myrick <mmyric4@illinois.edu>
2023-05-08 23:41:08 +0000

    save

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,19 @@
 Rails.application.routes.draw do
+  root "photos#index"
+  
+  devise_for :users
   resources :likes
   resources :follow_requests
   resources :comments
   resources :photos
-  devise_for :users
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root "photos#index"
+  get ":username/liked" => "photos#liked", as: :liked_photos
+
+  devise_scope :user do
+    get '/users/sign_out', to: 'devise/sessions#destroy'
+
+    get ":username" => "users#show", as: :user
+
+  end
 end
