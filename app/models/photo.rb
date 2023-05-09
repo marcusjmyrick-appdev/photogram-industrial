ae7e0fde61f1096a7fa638d2d039007fc51425a9
Marcus Myrick <mmyric4@illinois.edu>
2023-05-09 16:20:32 +0000

    100% completion

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,4 +1,17 @@
 class Photo < ApplicationRecord
   # belongs_to :owner
   belongs_to :owner, class_name: "User"
+  has_many :comments
+  has_many :likes
+  has_many :fans, through: :likes
+
+  validates :caption, presence: true
+  validates :image, presence: true
+
+  scope :by_likes, -> { order(likes_count: :desc) }
+  scope :past_week, -> { where(created_at: 1.week.ago...) }
+
+  def fan_list
+    @_fan_list ||= fans.pluck(:username).to_sentence
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,18 @@
 Rails.application.routes.draw do
   root "photos#index"
-  
+
+
   devise_for :users
+
   resources :likes
   resources :follow_requests
   resources :comments
   resources :photos
 
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get ":username/liked" => "photos#liked", as: :liked_photos
+  # get ":username/feed" => "photos#liked", as: :liked_photos
+  # get ":username/followers" => "photos#liked", as: :liked_photos
+  #  get ":username/following" => "photos#liked", as: :liked_photos
 
   devise_scope :user do
     get '/users/sign_out', to: 'devise/sessions#destroy'
@@ -16,4 +20,6 @@ Rails.application.routes.draw do
     get ":username" => "users#show", as: :user
 
   end
+
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
